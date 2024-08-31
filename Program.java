72dfdc68f14c262b4eec3774c1c3c6467ecad30a
Pawar187 <pawaranand187100@gmail.com>
2024-08-31 23:00:53 +0530

    second commit

--- Program.java
+++ Program.java
@@ -4,7 +4,16 @@ public class Program{
             System.out.print("Hello Java1");
 
       System.out.print("Hello World2");
+      Program p1=new Program();
+      p1.method();
       
   }
+
+  public void method() {
+            System.out.print("Hello Java from method");
+
+  }
+      
+  
 
 }
