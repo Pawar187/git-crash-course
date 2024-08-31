f24c3cefdeb5f9b7591b3912e499cb4bb16083a1
Pawar187 <pawaranand187100@gmail.com>
2024-08-31 22:56:22 +0530

    code added initial

---
+++ Program.java
@@ -0,0 +1,10 @@
+public class Program{
+  public static void main (String args[]){
+      System.out.print("Hello Java from VS");
+            System.out.print("Hello Java1");
+
+      System.out.print("Hello World2");
+      
+  }
+
+}
